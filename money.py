b714ef08b15da073d86e462921c41c3bbbb815b1
shicheju <1765939193@qq.com>
2021-03-18 15:52:51 +0800

    作业1

---
+++ money.py
@@ -0,0 +1,6 @@
+#-*-coding：utf-8-*-
+#@Time   :2021/3/18 14:45
+#@Author :shicheju
+#@File   :money.py
+
+saved_money=1000
---
+++ select_money.py
@@ -0,0 +1,9 @@
+#-*-coding：utf-8-*-
+#@Time   :2021/3/18 14:59
+#@Author :shicheju
+#@File   :select_money.py
+import money
+
+
+def select_money():
+    print("工资查询:",money.saved_money)
